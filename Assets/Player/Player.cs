94acfcd78b12341ffefcef493404e389695ae0e9
ArTeGaS-2 <probablytorvik@gmail.com>
2025-02-08 18:03:23 +0200

    Скрипт з логікою переміщення для персонажа гравця

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float forceMultiplier = 100f; // Множник швидкості
+    public float maxSpeed = 10f; // Максимальна швидкість
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody rb; // Фізичний компонент
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        Vector3 moveInput = new Vector3( // Набір з 3 координат
+            Input.GetAxis("Horizontal"), // X
+            0,  // Y
+            Input.GetAxis("Vertical")); // Z
+
+        if (moveInput.magnitude > 0.1f)
+        {
+            rb.AddForce(                // Прискорення об'єкта
+                moveInput.normalized * // Вектор руху
+                forceMultiplier *     // Множник швидкості
+                Time.deltaTime, ForceMode.Acceleration); // Узгодження і
+                                                         // тип прискорення
+            rb.velocity = Vector3.ClampMagnitude( // Обмеження швидкості
+                rb.velocity, // Те що обмежуємо
+                maxSpeed); // Те наскільки обмежуємо
+        }
     }
 }
